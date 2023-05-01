c6431990a56b41033eb8f995b4a9ceee1fd28b09
dikisetiawan1 <dikisetia101@gmail.comgit config --global user.email dikisetia101@gmail.comgit config --global user.email dikisetia101@gmail.com>
2023-05-01 15:31:04 +0700

    update page product detail checkbox

--- src/components/cardListDetail/index.jsx
+++ src/components/cardListDetail/index.jsx
@@ -1,5 +1,8 @@
+
 import { Container,Row,Col, Card } from "react-bootstrap";
 import '../cardListDetail/style.modules.css';
+import '../../helper/data';
+import DataMateri from "../../helper/data";
 
 
 const cardListDetail = () => {
@@ -13,7 +16,7 @@ return(
     <Row>
         <Col>
 
-        <Card className="mt-5 shadow " style={{width:'300px', height:'250px'}}>
+        <Card className="mt-5 shadow " style={{width:'300px', height:'270px', borderRadius:'20px'}}>
         <Card.Body>
             <Card.Title>
                 <div className="title-course">
@@ -22,6 +25,17 @@ return(
                 <p className="details-fas" style={{marginLeft:'40px'}}>Fasilitas</p>
 
                 <hr/>
+
+                <Row>
+                    <Col>
+                    <p style={{fontSize:'16px',color:'#6D7175'}}><del>Rp. 125.000</del></p>
+                    
+                    </Col>
+                    <Col>
+                    <p style={{fontSize:'20px',color:'#FF6A28'}}>Rp. 99.000</p>
+                    </Col>
+                  <a href="#" className="btn btn-warning text-white mt-4 " style={{borderRadius:'20px' , width:'80%', marginLeft:'30px'}}>Daftar Kelas</a>
+                </Row>
             </Card.Title>
             
         </Card.Body>        
@@ -31,10 +45,14 @@ return(
 
 
         <Col>
-        <Card className="mt-5 shadow" style={{width:'900px', height:'800px'}}>
+        <Card className="mt-5 shadow" style={{width:'900px', height:'650px', borderRadius:'20px'}}>
 
             <Card.Body>
-
+                <h3 style={{marginBottom:'25px', margin:'30px', marginLeft:'40px', fontWeight:'600'}}>Materi</h3>
+                <Card.Title>
+                <CardList arrydata={DataMateri} /> 
+             
+                </Card.Title>
             </Card.Body>
         </Card>
         
@@ -47,6 +65,37 @@ return(
 );
 
 
+
+
 }
+var CardList = function (props) {  
+    //ALl data Array  
+    var listDataTitle = props.arrydata.map(function (data) {  
+      return (
+       <>
+        
+      <div className="card-label mt-3" key={data.id}> 
+        <label className="checkbox-main">
+        <input  type="checkbox" for="checkbox" name="checkbox" id="checkbox"/>
+        <span className="check-box"></span>
+        </label>
+
+        <span style={{fontSize:'20px', fontWeight:'600', paddingLeft:'10px',position:'absolute', marginTop:'-25px'}}>{data.title} </span><br/> 
+        <span style={{fontSize:'14px',fontWeight:'400',color:'#868686',position:'absolute', marginTop:'-20px', marginRight:'60px', lineHeight:'1.5', paddingLeft:'10px'}}>{data.sub}</span></div>  
+        
+      </>
+      )});  
+    
+    return (  
+      <div style={{marginLeft:'65px', marginRight:'60px'}}>  
+    
+        { listDataTitle}
+       
+      </div>  
+    );  
+  }  
+
+
+ 
 
 export default cardListDetail;
---
+++ src/helper/data.js
@@ -0,0 +1,39 @@
+const DataMateri = [
+    {
+        id : '1',
+        title : 'Pengenalan Programming Laravel',
+        sub : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden.'
+    },
+    {
+        id : '2',
+        title : 'Materi Bootcamp Laravel 2',
+        sub : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden.'
+    },
+    {
+        id : '3',
+        title : 'Materi Bootcamp Laravel 3',
+        sub : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden.'
+    },
+    {
+        id : '4',
+        title : 'Materi Bootcamp Laravel 4',
+        sub : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden.'
+    },
+    {
+        id : '5',
+        title : 'Materi Bootcamp Laravel 5',
+        sub : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden.'
+    },
+    {
+        id : '6',
+        title : 'Materi Bootcamp Laravel 6',
+        sub : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden.'
+    },
+    
+]
+
+
+
+
+
+export default DataMateri;
